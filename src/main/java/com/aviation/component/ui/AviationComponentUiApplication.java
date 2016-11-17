6fc469979d5977b5127f62ef54d7ab790053e2bd
Amit Bansal <amitbansal26@gmail.com>
2016-11-17 08:48:15 +0530

    first commit

---
+++ src/main/java/com/aviation/component/ui/AviationComponentUiApplication.java
@@ -0,0 +1,12 @@
+package com.aviation.component.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviationComponentUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviationComponentUiApplication.class, args);
+	}
+}
