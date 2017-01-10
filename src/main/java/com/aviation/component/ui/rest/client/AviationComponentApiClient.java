65d3b9dc5a043ce121e16a74ce7d52f5d5f306c1
Veerappan <balaji.a.veerappan@capgemini.com>
2017-01-10 15:07:39 +0530

    after pushed to predix

--- src/main/java/com/aviation/component/ui/rest/client/AviationComponentApiClient.java
+++ src/main/java/com/aviation/component/ui/rest/client/AviationComponentApiClient.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.aviation.component.ui.vo.ComponentReport;
 import com.aviation.component.ui.vo.ComponentsPageVO;
-import com.aviation.component.ui.vo.RemovalReportVO;
+
+
+
 
 @FeignClient("aviation-component-api")
 public interface AviationComponentApiClient {
@@ -20,7 +22,12 @@ public interface AviationComponentApiClient {
 	@RequestMapping(value = "/removalReport", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ComponentReport getRemovalReport(@RequestParam("componentIds") List<Long> componentIds,@RequestParam("fromDate") String fromDate);
 
-	@RequestMapping(value="/navigationToRemoval", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ComponentsPageVO navigationToRemoval(@RequestParam("actualData") String actualData,@RequestParam("dataType") String dataType,@RequestParam("fromDate") String fromDate,@RequestParam("toDate") String toDate);
+	/*@RequestMapping(value="/navigationToRemoval", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ComponentsPageVO navigationToRemoval(@RequestParam("actualData") String actualData,@RequestParam("dataType") String dataType,@RequestParam("fromDate") String fromDate,@RequestParam("toDate") String toDate);*/
 
+	
+	@RequestMapping(value = "/navigationToRemoval", method =RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+	public ComponentsPageVO navigationToRemoval(@RequestParam("actualData") String actualData,@RequestParam("dataType") String dataType,@RequestParam("fromDate") String fromDate,@RequestParam("toDate") String toDate);
+	
+	
 }
